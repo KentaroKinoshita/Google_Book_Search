5a1b6bd5443bbf9acad42b492dd17572b0d03d1f
Kentaro Kinoshita <mixmix0321@gmail.com>
2021-01-16 01:11:33 -0500

    added saved function

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,34 +1,28 @@
 import React from "react";
 
-function Form({ q, handleInputChange, handleFormSubmit }) {
-  return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="Query">
-          <strong>Book</strong>
-        </label>
-        <input
-          className="form-control"
-          id="Title"
-          type="text"
-          value={q}
-          placeholder="Ready Player One"
-          name="q"
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div className="pull-right">
-        <button
-          onClick={handleFormSubmit}
-          type="submit"
-          className="btn btn-lg btn-danger float-right"
-        >
-          Search
+
+function Form(props){
+return (
+  <div className="container">
+  <form>
+    <div className="form-group">
+      <label htmlFor="search"><h2>Search for and save Books of Interest</h2></label>
+      <input
+        onChange={props.handleInputChange}
+        value={props.search}
+        name="search"
+        type="text"
+        className="form-control"
+        placeholder="Search a Book"
+        id="search"
+      />
+      <button onClick={props.handleFormSubmit} className="btn btn-dark mt-3 mb-5">
+        Search
         </button>
-      </div>
-    </form>
-  );
+    </div>
+  </form>
+  </div>
+);
 }
 
 export default Form;
